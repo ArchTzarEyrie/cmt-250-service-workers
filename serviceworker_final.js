17de7faab8581801a58371a4faf579516f0263a2
ArchTzarEyrie <zackreiter@protonmail.com>
2025-02-11 15:09:08 -0500

    add new files

---
+++ serviceworker_final.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (event) => {
+    console.log("[CHECK]: Service_Worker_Installing...")
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log("[CHECK]: Service_Worker_Activated");
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('message', (event) => {
+    console.log('Message Received');
+})
---
+++ serviceworker_main_finished.js
@@ -0,0 +1,27 @@
+let sw;
+
+function initServiceWorker() {
+  navigator.serviceWorker
+    .register("./serviceworker.js", { scope: "/" })
+    .then((registration) => {
+      console.log(
+        "[CHECK]: Service_Worker_Registered_With_Scope",
+        registration.scope
+      );
+      sw = registration;
+    })
+    .catch((error) => {
+      console.error("[ERR]: Service_Worker_Registration_Failed:", error);
+  });
+}
+  
+// Call the initServiceWorker function when the page loads
+window.addEventListener("load", () => {
+  initServiceWorker();
+});
+
+function buttonHandler(event) {
+  sw.active.postMessage('Button pushed');
+}
+
+document.getElementById('message-button').onclick = buttonHandler;
