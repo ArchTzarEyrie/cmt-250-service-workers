8cd49d673a797a1ff7f01df5fdaccff7fed53699
ArchTzarEyrie <zackreiter@protonmail.com>
2025-02-18 15:06:31 -0500

    add caching behavior

--- serviceworker_final.js
+++ serviceworker_final.js
@@ -8,8 +8,38 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
 
-self.addEventListener('message', (event) => {
+self.addEventListener('message', async (event) => {
     console.log('Message Received');
     console.log(event.data);
     event.source.postMessage('Message from SW to Client');
-})
+});
+
+async function handleFetch(event) {
+    const request = event.request;
+    const responseFromCache = await caches.match(request);
+
+    if (responseFromCache) {
+        console.log('Responding from cache');
+        return responseFromCache;
+    }
+
+    // Next try to get the resource from the network
+    try {
+
+        const responseFromNetwork = await fetch(request.clone());
+        const cache = await caches.open('v1')
+        cache.put(request, responseFromNetwork.clone());
+        console.log('Responding from network');
+        return responseFromNetwork;
+
+    } catch (error) {
+        
+        return new Response('Network error happened', {
+            status: 408,
+            headers: { 'Content-Type': 'text/plain' },
+        });
+    }
+}
+self.addEventListener('fetch', async (event) => {
+    event.respondWith(handleFetch(event));
+});
